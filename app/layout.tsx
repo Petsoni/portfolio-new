fcc9c1bcf679228c2c314cca1ffae2c2a1f9deaf
Petar <pmarkovic2308@gmail.com>
2026-04-30 21:06:31 +0200

    Fixed title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-import type {Metadata} from "next";
-import {Geist, Geist_Mono,} from "next/font/google";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./global.scss";
-import {Analytics} from "@vercel/analytics/next";
-import {TooltipProvider} from "@/components/ui/tooltip";
+import { Analytics } from "@vercel/analytics/next";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 const interTight = Geist({
   variable: "--font-geist-sans",
@@ -15,28 +15,26 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Petar Marković | Frontend Developer",
+  title: "Petar Marković | Software Engineer",
   description:
-    "Frontend Developer specializing in scalable web applications and enterprise systems. Experienced with Angular, React, Next.js, and modern development practices.",
+    "Software engineer specializing in scalable web applications and enterprise systems. Experienced with Angular, React, Next.js, and modern development practices.",
 };
 
 export default function RootLayout({
-                                     children,
-                                   }: Readonly<{
+  children,
+}: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-    <body
-      className={`${interTight.variable} ${geistMono.variable} antialiased`}
-    >
-    <main className={"w-[768px] mx-auto max-[769px]:w-[90%]"}>
-      <TooltipProvider>
-        {children}
-      </TooltipProvider>
-      <Analytics/>
-    </main>
-    </body>
+      <body
+        className={`${interTight.variable} ${geistMono.variable} antialiased`}
+      >
+        <main className={"w-[768px] mx-auto max-[769px]:w-[90%]"}>
+          <TooltipProvider>{children}</TooltipProvider>
+          <Analytics />
+        </main>
+      </body>
     </html>
   );
 }
